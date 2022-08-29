f70007b3ef8ec7f0a6dbc7fdaf0040b2f44f0aa3
kapiushon <amiller.swe@pm.me>
2022-08-28 23:00:25 -0400

    Personal: switching personal over to packer.nvim as well
    Former-commit-id: 6aabb907dc9ad3b4112845fa9039c0eabd671682

--- personal/.config/nvim/lua/load_plugins/null-ls_config.lua
+++
@@ -1,7 +0,0 @@
-local null_ls = require("null-ls")
-
-local sources = {
-   null_ls.builtins.formatting.black
-}
-
-null_ls.setup({ sources = sources })
--- personal/.config/nvim/lua/lsp_config/python_config.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-M.handlers = {
-   ["textDocument/hover"] = vim.lsp.with( vim.lsp.handlers.hover, { border = "rounded" }),
-   ["textDocument/signatureHelp"] = vim.lsp.with( vim.lsp.handlers.signature_help, { border = "rounded" }),
-}
-
-return M
