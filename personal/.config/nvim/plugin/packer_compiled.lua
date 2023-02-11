3b9dd06cd260b6baaa8963b43b8cfff83cfb5a1c
SuborbitalSentinel <amiller.swe@pm.me>
2023-02-11 10:40:15 -0500

    Personal: Switched to Nord

--- personal/.config/nvim/plugin/packer_compiled.lua
+++ personal/.config/nvim/plugin/packer_compiled.lua
@@ -139,6 +139,11 @@ _G.packer_plugins = {
     path = "/home/kapiushon/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  ["nord.nvim"] = {
+    loaded = true,
+    path = "/home/kapiushon/.local/share/nvim/site/pack/packer/start/nord.nvim",
+    url = "https://github.com/shaunsingh/nord.nvim"
+  },
   ["null-ls.nvim"] = {
     loaded = true,
     path = "/home/kapiushon/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
