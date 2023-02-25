e93ffe4fbac70311a631c942be9ad33c15b71ac7
SuborbitalSentinel <amiller.swe@pm.me>
2023-02-24 20:16:54 -0500

    Personal: changing personal dotfiles to make use of stow's --dotfile flag...but makes it not work with directories
    Former-commit-id: c6641a6365efd3566bd493ea9cd97c6e122cee7a

--- personal/.config/nvim/init.lua
+++ personal/.config/nvim/init.lua
@@ -9,3 +9,13 @@ vim.api.nvim_create_autocmd("BufReadPost", {
    command = "set bufhidden=delete"
 })
 
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    group = vim.api.nvim_create_augroup("XamlAuGroup", { clear = true }),
+    pattern = {"*.xaml", "*.axaml"},
+    callback = function()
+        vim.cmd[[setfiletype xml]]
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end
+})
