f308684c629df8aa1ea574a275ac430f3f872020
SuborbitalSentinel <amiller.swe@pm.me>
2023-04-06 11:22:32 -0400

    Personal: Lualine updates

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -48,6 +48,7 @@ return require('packer').startup(function(use)
             config = function()
                 require("everforest").setup({
                     background = "hard",
+                    transparent_background_level = 2,
                     italics = true
                 })
             end
