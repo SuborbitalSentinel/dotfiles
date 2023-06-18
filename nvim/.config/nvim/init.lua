3990840a858ed3f15881b1713dd4fc15c48d8daa
SuborbitalSentinel <amiller.swe@pm.me>
2023-06-18 10:35:54 -0400

    Personal: Trying out tabs instead of spaces
    Former-commit-id: 71c19c57e6c39cb191a68f18d8a8eeca95597acb

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,23 +10,23 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 })
 
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-    group = vim.api.nvim_create_augroup("GolangAuGroup", { clear = true }),
-    pattern = {"*.go"},
-    callback = function()
-        vim.opt_local.tabstop = 4
-        vim.opt_local.softtabstop = 4
-        vim.opt_local.shiftwidth = 4
-        vim.opt_local.expandtab = false
-    end
+	group = vim.api.nvim_create_augroup("GolangAuGroup", { clear = true }),
+	pattern = {"*.go"},
+	callback = function()
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.expandtab = false
+	end
 })
 
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-    group = vim.api.nvim_create_augroup("XamlAuGroup", { clear = true }),
-    pattern = {"*.xaml", "*.axaml"},
-    callback = function()
-        vim.cmd[[setfiletype xml]]
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
-        vim.opt_local.shiftwidth = 2
-    end
+	group = vim.api.nvim_create_augroup("XamlAuGroup", { clear = true }),
+	pattern = {"*.xaml", "*.axaml"},
+	callback = function()
+		vim.cmd[[setfiletype xml]]
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end
 })
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -3,69 +3,68 @@ local packer_bootstrap = nil
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-        install_path })
-    vim.cmd [[packadd packer.nvim]]
+	packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+		install_path })
+	vim.cmd [[packadd packer.nvim]]
 end
 
 return require('packer').startup(function(use)
-    use {
-        'wbthomason/packer.nvim',
+	use {
+		'wbthomason/packer.nvim',
 
-        'Decodetalkers/csharpls-extended-lsp.nvim',
-        'L3MON4D3/LuaSnip',
-        'shaunsingh/nord.nvim',
-        'godlygeek/tabular',
-        'hoob3rt/lualine.nvim',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-calc',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-nvim-lsp-signature-help',
-        'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/cmp-path',
-        'hrsh7th/nvim-cmp',
-        'j-hui/fidget.nvim',
-        'jose-elias-alvarez/null-ls.nvim',
-        'kyazdani42/nvim-web-devicons',
-        'lewis6991/gitsigns.nvim',
-        'neovim/nvim-lspconfig',
-        'norcalli/nvim-colorizer.lua',
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope-ui-select.nvim',
-        'nvim-treesitter/playground',
-        'tpope/vim-eunuch',
-        'tpope/vim-fugitive',
-        'tpope/vim-repeat',
-        'tpope/vim-surround',
-        'tpope/vim-unimpaired',
-        'tpope/vim-vinegar',
-        'williamboman/mason-lspconfig.nvim',
-        'williamboman/mason.nvim',
-        'windwp/nvim-autopairs',
-        {
-            'numToStr/Comment.nvim',
-            config = function()
-                require('Comment').setup()
-            end
-        },
-        {
-            'neanias/everforest-nvim',
-            config = function()
-                require("everforest").setup({
-                    background = "hard",
-                    transparent_background_level = 2,
-                    italics = true
-                })
-            end
-        },
-        { "rose-pine/neovim", as = "rose-pine" },
-        { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 },
-        { 'nvim-telescope/telescope.nvim', tag = '0.1.0', },
-        { 'nvim-treesitter/nvim-treesitter', run = function() vim.cmd [[TSUpdate]] end },
-        { 'aymericbeaumet/vim-symlink', requires = { 'moll/vim-bbye' } },
-    }
+		'Decodetalkers/csharpls-extended-lsp.nvim',
+		'L3MON4D3/LuaSnip',
+		'shaunsingh/nord.nvim',
+		'godlygeek/tabular',
+		'hoob3rt/lualine.nvim',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-calc',
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-nvim-lsp-signature-help',
+		'hrsh7th/cmp-nvim-lua',
+		'hrsh7th/cmp-path',
+		'hrsh7th/nvim-cmp',
+		'j-hui/fidget.nvim',
+		'jose-elias-alvarez/null-ls.nvim',
+		'kyazdani42/nvim-web-devicons',
+		'lewis6991/gitsigns.nvim',
+		'neovim/nvim-lspconfig',
+		'norcalli/nvim-colorizer.lua',
+		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope-ui-select.nvim',
+		'nvim-treesitter/playground',
+		'tpope/vim-eunuch',
+		'tpope/vim-fugitive',
+		'tpope/vim-repeat',
+		'tpope/vim-surround',
+		'tpope/vim-unimpaired',
+		'tpope/vim-vinegar',
+		'williamboman/mason-lspconfig.nvim',
+		'williamboman/mason.nvim',
+		'windwp/nvim-autopairs',
+		{
+			'numToStr/Comment.nvim',
+			config = function()
+				require('Comment').setup()
+			end
+		},
+		{
+			'neanias/everforest-nvim',
+			config = function()
+				require("everforest").setup({
+					background = "hard",
+					transparent_background_level = 2,
+					italics = true
+				})
+			end
+		},
+		{ "rose-pine/neovim", as = "rose-pine" },
+		{ 'nvim-telescope/telescope.nvim', tag = '0.1.0', },
+		{ 'nvim-treesitter/nvim-treesitter', run = function() vim.cmd [[TSUpdate]] end, requires = { {'natecraddock/telescope-zf-native.nvim'}} },
+		{ 'aymericbeaumet/vim-symlink', requires = { 'moll/vim-bbye' } },
+	}
 
-    if packer_bootstrap ~= nil then
-        require('packer').sync()
-    end
+	if packer_bootstrap ~= nil then
+		require('packer').sync()
+	end
 end)
