f2b96b3e08a77264f070487b362690a2877fb899
SuborbitalSentinel <amiller.swe@pm.me>
2023-08-27 14:36:57 -0400

    Personal: Don't even remember what I did here tbh
    Former-commit-id: 6bad40bc1eed9cd58e6f9abe01c1e0d4f135215b

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,32 +1,43 @@
-require('globals')
-require('plugins')
-require('settings')
-require('keymaps')
-require('lsp_config')
+require("globals")
+require("plugins")
+require("settings")
+require("keymaps")
+require("lsp_config")
 
 vim.api.nvim_create_autocmd("BufReadPost", {
-   pattern = { "fugitive://*", "term://*" },
-   command = "set bufhidden=delete"
+	pattern = { "fugitive://*", "term://*" },
+	command = "set bufhidden=delete",
 })
 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 	group = vim.api.nvim_create_augroup("GolangAuGroup", { clear = true }),
-	pattern = {"*.go"},
+	pattern = { "*.go" },
 	callback = function()
 		vim.opt_local.tabstop = 4
 		vim.opt_local.softtabstop = 4
 		vim.opt_local.shiftwidth = 4
 		vim.opt_local.expandtab = false
-	end
+	end,
 })
 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 	group = vim.api.nvim_create_augroup("XamlAuGroup", { clear = true }),
-	pattern = {"*.xaml", "*.axaml"},
+	pattern = { "*.xaml", "*.axaml" },
 	callback = function()
-		vim.cmd[[setfiletype xml]]
+		vim.cmd([[setfiletype xml]])
 		vim.opt_local.tabstop = 2
 		vim.opt_local.softtabstop = 2
 		vim.opt_local.shiftwidth = 2
-	end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	group = vim.api.nvim_create_augroup("HtmlAuGroup", { clear = true }),
+	pattern = { "*.html", "*.gotmpl" },
+	callback = function()
+		vim.cmd([[setfiletype html]])
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
 })
--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -75,7 +75,7 @@ time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["Comment.nvim"] = {
-    config = { "\27LJ\2\0025\0\0\2\0\3\0\0066\0\0\0'\1\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0" },
+    config = { "\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0" },
     loaded = true,
     path = "/home/adam/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
@@ -145,6 +145,11 @@ _G.packer_plugins = {
     path = "/home/adam/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  ["moq-swap"] = {
+    loaded = true,
+    path = "/home/adam/.local/share/nvim/site/pack/packer/start/moq-swap",
+    url = "/home/adam/plugins/moq-swap"
+  },
   ["nord.nvim"] = {
     loaded = true,
     path = "/home/adam/.local/share/nvim/site/pack/packer/start/nord.nvim",
@@ -184,6 +189,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/adam/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
+  ["omnisharp-extended-lsp.nvim"] = {
+    loaded = true,
+    path = "/home/adam/.local/share/nvim/site/pack/packer/start/omnisharp-extended-lsp.nvim",
+    url = "https://github.com/Hoffs/omnisharp-extended-lsp.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
@@ -275,7 +285,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: Comment.nvim
 time([[Config for Comment.nvim]], true)
-try_loadstring("\27LJ\2\0025\0\0\2\0\3\0\0066\0\0\0'\1\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
+try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
 
 _G._packer.inside_compile = false
