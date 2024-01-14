6be706a37ace2fc2ca446c63af5f83575db86abc
SuborbitalSentinel <amiller.swe@pm.me>
2024-01-13 22:46:46 -0500

    personal: moving plugin configs to their own files
    Former-commit-id: 8ea3094596b9c222092c3fd7a5520770163caca0

---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,26 @@
+return {
+	"godlygeek/tabular",
+	"tpope/vim-eunuch",
+	"tpope/vim-repeat",
+	"tpope/vim-surround",
+	"tpope/vim-unimpaired",
+
+	{ "kyazdani42/nvim-web-devicons", lazy = true },
+
+	{ "aymericbeaumet/vim-symlink", dependencies = { "moll/vim-bbye" } },
+
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function() require("colorizer").setup() end,
+	},
+
+	{
+		"windwp/nvim-autopairs",
+		config = function() require("nvim-autopairs").setup() end,
+	},
+
+	{
+		"numToStr/Comment.nvim",
+		config = function() require("Comment").setup() end,
+	},
+}
---
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,9 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"Hoffs/omnisharp-extended-lsp.nvim",
+		"stevearc/conform.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"williamboman/mason.nvim",
+	},
+}
