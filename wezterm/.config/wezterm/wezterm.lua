cac62c600935b0a12aa0d15fd7be99d3d1fb921c
SuborbitalSentinel <amiller.swe@pm.me>
2024-03-14 20:50:22 -0400

    Personal: dotfile updates

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,112 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.enable_wayland = false
+config.front_end="WebGpu"
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 12
+
+config.colors = require("lua/rose-pine-moon").colors()
+config.window_frame = require("lua/rose-pine-moon").window_frame()
+config.window_decorations = "RESIZE"
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.default_prog = { "bash" }
+
+config.leader = { key = "z", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "F11",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "o",
+		mods = "LEADER",
+		action = wezterm.action.RotatePanes('Clockwise')
+	},
+	{
+		key = "o",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.RotatePanes('CounterClockwise')
+	},
+	{
+		key = "h",
+		mods = "ALT",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		key = "j",
+		mods = "ALT",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "k",
+		mods = "ALT",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "l",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "h",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Left",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "j",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "k",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Up",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+}
+
+return config
